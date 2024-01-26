e2485b191f09604e612d02c5a39a171b95649bdf
MercylineAbwori <mercylineachieng99@gmail.com>
2024-01-26 16:55:39 +0300

    pushed

--- lib/components/pickers/file_picker/file_picker.dart
+++ lib/components/pickers/file_picker/file_picker.dart
@@ -6,6 +6,8 @@ import 'package:dotted_border/dotted_border.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_template/shared/download_file/download_progress_dialog.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class FilePickerPage extends StatefulWidget {
   
@@ -60,100 +62,167 @@ class _FilePickerPageState extends State<FilePickerPage> {
             Card(
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: DottedBorder(
-                  child: Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center, 
-                      children: [
-                        (result != null || file != null)?
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const SizedBox(height: 10,),
-                                ListView.separated(
-                                    shrinkWrap: true,
-                                    itemCount: result?.files.length ?? 0,
-                                    itemBuilder: (context, index) {
-                                      return Column(
-                                        children: [
-                                          Center(
-                                            child: Container(
-                                              height: 120,
-                                              width: 150,
-                                              child: (kIsWeb)?
-                                                  Image.memory(
-                                                    result!.files.first.bytes!,
-                                                    height: 350,
-                                                    fit: BoxFit.fill,
-                                                  ): Image.file(file!, height: 150, width: 150, fit: BoxFit.fill),
-                                            ),
-                                          ),
-                                          Center(
-                                            child: Container(
-                                              height: 30,
-                                              width: 150,
-                                              color: Colors.grey,
-                                              child: Padding(
-                                                padding: const EdgeInsets.all(5.0),
-                                                child: Center(
-                                                  child: Text(result?.files[index].name ?? '',
-                                                      style: const TextStyle(
-                                                          fontSize: 13, fontWeight: FontWeight.bold)),
+                child: Column(
+                  children: [
+                    DottedBorder(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center, 
+                          children: [
+                            (result != null || file != null)?
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const SizedBox(height: 10,),
+                                    ListView.separated(
+                                        shrinkWrap: true,
+                                        itemCount: result?.files.length ?? 0,
+                                        itemBuilder: (context, index) {
+                                          return ListTile(
+                                            // onTap: () => OpenAppFile.open(result?.files[index].path.toString()),
+
+                                            title:
+                                                Column(
+                                              children: [
+                                                const SizedBox(
+                                                  height: 10,
                                                 ),
-                                              ),
+                                                const Icon(
+                                                  Icons.file_open,
+                                                  size: 50,
+                                                ),
+                                                const SizedBox(
+                                                  width: 10,
+                                                ),
+                                                Text(result?.files[index].name ?? '',
+                                                    style: const TextStyle(
+                                                      fontSize: 16,
+                                                      fontWeight: FontWeight.bold,
+                                                    )),
+                                                const SizedBox(
+                                                  height: 10,
+                                                ),
+                                              ],
                                             ),
-                                          ),
-                                        ],
-                                      );
-                                    }, separatorBuilder: (BuildContext context, int index) { 
-                                      return const SizedBox(height: 5,);
-                                    },)
-                              ],
-                            ),
-                          ):
-                          const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                
-                                SizedBox(height: 10,),
-                                Center(
-                                  child: Text("No File Selected",
-                                      style: TextStyle(
-                                          fontSize: 13, fontWeight: FontWeight.bold)),
+                                          );
+                                          
+                                        }, separatorBuilder: (BuildContext context, int index) { 
+                                          return const SizedBox(height: 5,);
+                                        },)
+                                  ],
                                 ),
-                              ],
-                            ),
-                          ),
-                        
-                        
-                        Center(
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              try {
-                                result = await FilePicker.platform.pickFiles();
-                                if (result != null) {
-                                  if (!kIsWeb) {
-                                    file = File(result!.files.single.path!);
-                                  }
-                                  setState(() {});
-                                } else {
-                                  print("No file selected");
+                              ):
+                              const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    
+                                    SizedBox(height: 10,),
+                                    Center(
+                                      child: Text("No File Selected",
+                                          style: TextStyle(
+                                              fontSize: 13, fontWeight: FontWeight.bold)),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            
+                          ]
+                        )
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            try {
+                              result = await FilePicker.platform.pickFiles();
+                              if (result != null) {
+                                if (!kIsWeb) {
+                                  file = File(result!.files.single.path!);
                                 }
-                              } catch (_) {}
-                            },
-                            child: const Text('Pick File'),
-                          ),
+                                setState(() {});
+                              } else {
+                                print("No file selected");
+                              }
+                            } catch (_) {}
+                          },
+                          child: const Text('Upload File'),
                         ),
-                      ]
-                    )
-                  ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            bool result = await _permissionRequest();
+                            if (result) {
+                              showDialog(
+                                  context: context,
+                                  builder: (dialogcontext) {
+                                    return DownloadProgressDialog();
+                                  });
+                            } else {
+                              print("No permission to read and write.");
+                            }
+                          },
+                          child: const Text('Download File'),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            try {
+                              result = await FilePicker.platform.pickFiles();
+                              if (result != null) {
+                                if (!kIsWeb) {
+                                  file = File(result!.files.single.path!);
+                                }
+                                setState(() {});
+                              } else {
+                                print("No file selected");
+                              }
+                            } catch (_) {}
+                          },
+                          child: const Text('Preview File'),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            try {
+                              result = await FilePicker.platform.pickFiles();
+                              if (result != null) {
+                                if (!kIsWeb) {
+                                  file = File(result!.files.single.path!);
+                                }
+                                setState(() {});
+                              } else {
+                                print("No file selected");
+                              }
+                            } catch (_) {}
+                          },
+                          child: const Text('Upload file to server'),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -163,6 +232,16 @@ class _FilePickerPageState extends State<FilePickerPage> {
       ),
 
     );
+  }
+
+  static Future<bool> _permissionRequest() async {
+    PermissionStatus result;
+    result = await Permission.storage.request();
+    if (result.isGranted) {
+      return true;
+    } else {
+      return false;
+    }
   }
  
 }
